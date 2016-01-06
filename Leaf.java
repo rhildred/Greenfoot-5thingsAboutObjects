14a5f35f485fe4d1221a122118f54b562a80615d
unknown <rhildred@SCJB23-18.ad.wlu.ca>
2016-01-06 12:32:24 -0500

    added wombats project

---
+++ Leaf.java
@@ -0,0 +1,11 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
+
+/**
+ * Leaf - a class for representing leafs.
+ *
+ * @author Michael Kolling
+ * @version 1.0.1
+ */
+public class Leaf extends Food
+{
+}
---
+++ PoisonousShroom.java
@@ -0,0 +1,14 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class PoisonousShroom here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PoisonousShroom extends Food
+{
+    public boolean bPoisonous(){
+        return true;
+    }
+}
